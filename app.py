d046d593b9086e20ecfb0593e108a86a626581b7
mutuku muli <mutukumuli9@gmail.com>
2021-10-31 23:21:23 +0300

    login page

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.env = "Development"
+app.debug = "True"
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('login.html')
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run()
