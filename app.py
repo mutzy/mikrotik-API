0233b274533186492d5f1f053d9283b68e23d575
mutuku muli <mutukumuli9@gmail.com>
2021-11-01 11:44:46 +0300

    Streamlined login page route

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from datetime import date
 
 app = Flask(__name__)
 app.env = "Development"
 app.debug = "True"
 
-@app.route("/")
-def hello_world():
-    return render_template('login.html')
+# creating the date object of today's date
+todays_date = date.today()
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route("/login")
+def login():
+    date = todays_date.year
+    return render_template('login.html',date=date)
 
 if __name__ == '__main__':
    app.run()
