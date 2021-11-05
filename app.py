a200c9eb0edae77601b27e9baae0e1421be79d2a
mutuku muli <mutukumuli9@gmail.com>
2021-11-05 14:48:01 +0300

    Added session

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import sys, posix, time, binascii, socket, select, ssl
 import hashlib
 from flask import Flask, render_template, redirect, url_for, request, session
+from flask_session import Session
 from datetime import date
 import ssl
 from librouteros import connect
@@ -13,7 +14,9 @@ app = Flask(__name__)
 app.env = "Development"
 app.debug = "True"
 app.config['SECRET_KEY'] = '0af158fc013329a0492cc929d3fbee02'
-
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # creating the date object of today's date
 todays_date = date.today()
@@ -21,14 +24,21 @@ todays_date = date.today()
 
 @app.route('/')
 def index():
-    if 'username' in session:
+    if session.get('username'):
         return render_template('index.html',username = session["username"])
     return redirect(url_for('login'))
 
 @app.route("/login",methods=['POST','GET'])
 def login():
     date = todays_date.year
-    if request.method == 'POST':
+    if request.method == 'POST' and len(request.form['username']) > 0:
+        # user = User.query.get(form.email.data)
+        # if user:
+        #     if bcrypt.check_password_hash(user.password, form.password.data):
+        #         user.authenticated = True
+        #         db.session.add(user)
+        #         db.session.commit()
+        #         login_user(user, remember=True)
         session['username'] = request.form['username']
         #api = connect(username='admin', password='', host='127.0.0.1')
         #ip_info = api(cmd="/ip/address/print")
